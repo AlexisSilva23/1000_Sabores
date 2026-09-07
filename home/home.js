c771ef2825449ad2d62e1878675de565de039703
Ale_swk <ale.silvah@duocuc.cl>
2026-09-07 14:19:22 -0300

    actualizacion tarjetas productos

--- home/home.js
+++ home/home.js
@@ -1,33 +1,22 @@
 console.log("Holaaa");
 
-const productos = [
-    {
-        codigo : "TC001",
-        nombre : "Torta Cuadrada de Chocolate",
-        precio : 45000,
-        imagen : "../img/TC001.png",
-        categoria : "Tortas Cuadradas"
-    },
-    {
-        codigo : "TC002",
-        nombre : "Torta Cuadrada Frutas",
-        precio : 50000,
-        imagen : "../img/TC002.png",
-        categoria : "Tortas Cuadradas"
-    },
-    {
-        codigo : "TT001",
-        nombre : "Torta Redonda de Vainilla",
-        precio : 40000,
-        imagen : "../img/TT001.png",
-        categoria : "Tortas Redondas"
-    },
-    {
-        codigo : "PI001",
-        nombre : "Mousse de Chocolate",
-        precio : 5000,
-        imagen : "../img/PI001.png",
-        categoria : "Postres Individuales"
-    }
-]
-    
+const contenedor = document.querySelector("#contenedor-productos-home");
+
+const mostrarProductos = productos.slice(0, 4);  // ← Los 4 primeros
+
+mostrarProductos.forEach(function(producto) {
+  const tarjeta = `
+    <div class="col-md-3">
+      <div class="card">
+        <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+        <div class="card-body">
+          <h5 class="card-title">${producto.nombre}</h5>
+          <p class="card-text">$${producto.precio.toLocaleString()}</p>
+          <button class="btn btn-outline-success">Añadir al carrito</button>
+        </div>
+      </div>
+    </div>
+  `;
+  
+  contenedor.innerHTML += tarjeta;
+});
