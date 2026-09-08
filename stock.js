6885fdfaa5bd5be6ebaf4678ba09387369d4c2e0
Ale_swk <ale.silvah@duocuc.cl>
2026-09-08 16:59:11 -0300

    productos html listo

---
+++ stock.js
@@ -0,0 +1,116 @@
+console.log("Holaa");
+
+const productos = [
+    {
+        codigo : "TC001",
+        nombre : "Torta Cuadrada de Chocolate",
+        precio : 45000,
+        imagen : "../img/TC001.png",
+        categoria : "Tortas Cuadradas"
+    },
+    {
+        codigo : "TC002",
+        nombre : "Torta Cuadrada de Frutas",
+        precio : 50000,
+        imagen : "../img/TC002.png",
+        categoria : "Tortas Cuadradas"
+    },
+    {
+        codigo : "TT001",
+        nombre : "Torta Circular de Vainilla",
+        precio : 40000,
+        imagen : "../img/TT001.png",
+        categoria : "Tortas Circulares"
+    },
+    {
+        codigo : "TT002",
+        nombre : "Torta Circular de Manjar",
+        precio : 42000,
+        imagen : "../img/TT002.png",
+        categoria : "Postres Circulares"
+    },
+    {
+        codigo : "PI001",
+        nombre : "Mousse de Chocolate",
+        precio : 5000,
+        imagen : "../img/PI001.png",
+        categoria : "Postres Individuales"
+    },
+    {
+        codigo : "PI002",
+        nombre: "Tiramisu Clasico",
+        precio : 5500,
+        imagen : "../img/PI002.png",
+        categoria : "Postres Individuales"
+    },
+    {
+        codigo : "PSA001",
+        nombre : "Torta sin azucar de Naranja",
+        precio : 48000,
+        imagen : "../img/PSA001.png",
+        categoria : "Productos sin Azucar"
+    },
+    {
+        codigo : "PSA002",
+        nombre : "Cheesecake sin azucar",
+        precio : 47000,
+        imagen : "../img/PSA002.png",
+        categoria : "Productos sin Azucar"
+    },
+    {
+        codigo : "PT001",
+        nombre : "Empanada de Manzana",
+        precio : 3000,
+        imagen : "../img/PT001.png",
+        categoria : "Pasteleria Tradicional"
+    },
+    {
+        codigo : "PT002",
+        nombre : "Tarta de Santiago",
+        precio : 6000,
+        imagen : "../img/PT002.png",
+        categoria : "Pasteleria Tradicional"
+    },
+    {
+        codigo : "PG001",
+        nombre : "Brownie sin gluten",
+        precio : 4000,
+        imagen : "../img/PG001.png",
+        categoria : "Productos sin Gluten"
+    },
+    {
+        codigo : "PG002",
+        nombre : "Pan sin gluten",
+        precio : 3500,
+        imagen : "../img/PG002.png",
+        categoria : "Productos sin Gluten"
+    },
+    {
+        codigo : "PV001",
+        nombre : "Torta Vegana de Chocolate",
+        precio : 50000,
+        imagen : "../img/PV001.png",
+        categoria : "Productos Veganos"
+    },
+    {
+        codigo : "PV002",
+        nombre : "Galletas Veganas de Avena",
+        precio : 4500,
+        imagen : "../img/PV002.png",
+        categoria : "Productos Veganos"
+    },
+    {
+        codigo : "TE001",
+        nombre : "Torta Especial de Cumpleaños",
+        precio : 55000,
+        imagen : "../img/TE001.png",
+        categoria : "Tortas Especiales"
+    },
+    {
+        codigo : "TE002",
+        nombre : "Torta Especial de Bodas",
+        precio : 60000,
+        imagen : "../img/TE002.png",
+        categoria : "Tortas Especiales"
+    }
+]
