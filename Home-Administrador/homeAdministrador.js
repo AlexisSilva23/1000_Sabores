6c06aadda55b2c8cd6ade4cb501525a0a9f04dcd
MR0111 <joaqu.rodriguezr@duocuc.cl>
2026-09-08 20:38:01 -0300

    Actualiza estructura HTML y CSS del panel de pedidos

--- Home-Administrador/homeAdministrador.js
+++ Home-Administrador/homeAdministrador.js
@@ -1,26 +1,3 @@
-.Titulo_Administrador{
-   display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.marco_principal {
-  border: 2px solid #555;
-}
-
-
-.linea_vertical_menu {
-  border-right: 1px solid #777;
-}
-
-
-.llinea_horizontal_panel {
-  border-bottom: 1px solid #777;
-}
-
-
-.linea-menu {
-  border: 0;
-  border-top: 1px solid #aaa;
-  margin: 15px 0;
-}
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Panel de administración cargado correctamente.');
+});
